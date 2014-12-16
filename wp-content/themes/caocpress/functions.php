43fdb39e31d3c22c6e6e4873d727367954242816
James Acklin <james@acklindesign.com>
2014-12-15 19:23:30 -0500

    Add dynamic menu

--- wp-content/themes/caocpress/functions.php
+++ wp-content/themes/caocpress/functions.php
@@ -1,98 +1,98 @@
 <?php
-	/**
-	 * Starkers functions and definitions
-	 *
-	 * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
-	 *
- 	 * @package 	WordPress
- 	 * @subpackage 	Starkers
- 	 * @since 		Starkers 4.0
-	 */
+  /**
+   * Starkers functions and definitions
+   *
+   * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+   *
+   * @package   WordPress
+   * @subpackage  Starkers
+   * @since     Starkers 4.0
+   */
 
-	/* ========================================================================================================================
+  /* ========================================================================================================================
 
-	Required external files
+  Required external files
 
-	======================================================================================================================== */
+  ======================================================================================================================== */
 
-	require_once( 'external/starkers-utilities.php' );
+  require_once( 'external/starkers-utilities.php' );
 
-	/* ========================================================================================================================
+  /* ========================================================================================================================
 
-	Theme specific settings
+  Theme specific settings
 
-	Uncomment register_nav_menus to enable a single menu with the title of "Primary Navigation" in your theme
+  Uncomment register_nav_menus to enable a single menu with the title of "Primary Navigation" in your theme
 
-	======================================================================================================================== */
+  ======================================================================================================================== */
 
-	add_theme_support('post-thumbnails');
+  add_theme_support('post-thumbnails');
 
-	// register_nav_menus(array('primary' => 'Primary Navigation'));
+  register_nav_menus(array('primary' => 'Main Navigation'));
 
-	/* ========================================================================================================================
+  /* ========================================================================================================================
 
-	Actions and Filters
+  Actions and Filters
 
-	======================================================================================================================== */
+  ======================================================================================================================== */
 
-	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
+  add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
 
-	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
+  add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
 
-	/* ========================================================================================================================
+  /* ========================================================================================================================
 
-	Custom Post Types - include custom post types and taxonimies here e.g.
+  Custom Post Types - include custom post types and taxonimies here e.g.
 
-	e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
+  e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
 
-	======================================================================================================================== */
+  ======================================================================================================================== */
 
 
 
-	/* ========================================================================================================================
+  /* ========================================================================================================================
 
-	Scripts
+  Scripts
 
-	======================================================================================================================== */
+  ======================================================================================================================== */
 
-	/**
-	 * Add scripts via wp_head()
-	 *
-	 * @return void
-	 * @author Keir Whitaker
-	 */
+  /**
+   * Add scripts via wp_head()
+   *
+   * @return void
+   * @author Keir Whitaker
+   */
 
-	function starkers_script_enqueuer() {
+  function starkers_script_enqueuer() {
 
-		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
+    wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
         wp_enqueue_style( 'screen' );
-	}
+  }
 
-	/* ========================================================================================================================
+  /* ========================================================================================================================
 
-	Comments
+  Comments
 
-	======================================================================================================================== */
+  ======================================================================================================================== */
 
-	/**
-	 * Custom callback for outputting comments
-	 *
-	 * @return void
-	 * @author Keir Whitaker
-	 */
-	function starkers_comment( $comment, $args, $depth ) {
-		$GLOBALS['comment'] = $comment;
-		?>
-		<?php if ( $comment->comment_approved == '1' ): ?>
-		<li>
-			<article id="comment-<?php comment_ID() ?>">
-				<?php echo get_avatar( $comment ); ?>
-				<h4><?php comment_author_link() ?></h4>
-				<time><a href="#comment-<?php comment_ID() ?>" pubdate><?php comment_date() ?> at <?php comment_time() ?></a></time>
-				<?php comment_text() ?>
-			</article>
-		<?php endif;
-	}
+  /**
+   * Custom callback for outputting comments
+   *
+   * @return void
+   * @author Keir Whitaker
+   */
+  function starkers_comment( $comment, $args, $depth ) {
+    $GLOBALS['comment'] = $comment;
+    ?>
+    <?php if ( $comment->comment_approved == '1' ): ?>
+    <li>
+      <article id="comment-<?php comment_ID() ?>">
+        <?php echo get_avatar( $comment ); ?>
+        <h4><?php comment_author_link() ?></h4>
+        <time><a href="#comment-<?php comment_ID() ?>" pubdate><?php comment_date() ?> at <?php comment_time() ?></a></time>
+        <?php comment_text() ?>
+      </article>
+    <?php endif;
+  }
 
 
 add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
@@ -126,7 +126,7 @@ function the_breadcrumb() {
                 echo $output;
                 echo '<li><strong title="'.$title.'"> '.$title.'</strong></li>';
             } else {
-            		echo '<li><a href="/">Home</a></li>';
+                echo '<li><a href="/">Home</a></li>';
                 echo '<li><strong> '.get_the_title().'</strong></li>';
             }
 
@@ -148,13 +148,13 @@ function the_breadcrumb() {
  */
 function arphabet_widgets_init() {
 
-	register_sidebar( array(
-		'name' => 'Global Footer',
-		'id' => 'global-footer',
-		'before_widget' => '<div class="section">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	) );
+  register_sidebar( array(
+    'name' => 'Global Footer',
+    'id' => 'global-footer',
+    'before_widget' => '<div class="section">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3>',
+    'after_title' => '</h3>',
+  ) );
 }
 add_action( 'widgets_init', 'arphabet_widgets_init' );
--- wp-content/themes/caocpress/parts/shared/header.php
+++ wp-content/themes/caocpress/parts/shared/header.php
@@ -1,13 +1,18 @@
-	<header role="banner">
-		<div class="container-fluid">
-			<ul id="primary-nav" class="nav" role="navigation">
-				<li><a href="<?php echo home_url(); ?>" class="logo">
-					<img src="<?= get_template_directory_uri().'/images/logo-combined.svg' ?>" alt="<?php bloginfo( 'name' ); ?>"></a>
-				</li>
-				<li><a href="/about">About</a></li>
-				<li><a href="/news">News</a></li>
-				<li><a href="/toolkit">Toolkit</a></li>
-				<li><a href="/contact">Contact</a></li>
-			</ul>
-		</div>
-	</header>
+  <header role="banner">
+    <div class="container-fluid">
+      <ul id="primary-nav" class="nav" role="navigation">
+        <li><a href="<?php echo home_url(); ?>" class="logo">
+          <img src="<?= get_template_directory_uri().'/images/logo-combined.svg' ?>" alt="<?php bloginfo( 'name' ); ?>"></a>
+        </li>
+        <?php
+          $menuargs = array(
+            'menu' => '1',
+            'container' => false,
+            'items_wrap' => '%3$s',
+          );
+          wp_nav_menu( $menuargs );
+        ?>
+
+      </ul>
+    </div>
+  </header>
