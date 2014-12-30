952dc838bd89f7287c884c33d314f3c37f5f8880
James Acklin <james@acklindesign.com>
2014-12-29 20:57:06 -0500

    Homepage video looping

--- wp-content/themes/caocpress/js/main.js
+++ wp-content/themes/caocpress/js/main.js
@@ -1,39 +1,41 @@
 jQuery(document).ready(function($){
 
-	$('.nav li:not(:first)').addClass('hidden');
+  $('.nav li:not(:first)').addClass('hidden');
 
-	// Insert hamburger menu into nav. Draws with SVG if Modernizr detects support; falls back to the old icon font thing if no SVG support.
-	if (Modernizr.inlinesvg){
-	  $('<a class="nav-button btn" href="#primary-nav"><svg width="15" height="15"><path d="M0,5 15,5" stroke="#FDFFE3" stroke-width="2"/><path d="M0,9 15,9" stroke="#FDFFE3" stroke-width="2"/><path d="M0,13 15,13" stroke="#FDFFE3" stroke-width="2"/></svg></a>').insertBefore('#primary-nav');
-	} else {
-	  $('<a class="nav-button btn" href="#primary-nav"><i class="icon-reorder"></i></a>').insertBefore('#primary-nav');
-	}
+  // Insert hamburger menu into nav. Draws with SVG if Modernizr detects support; falls back to the old icon font thing if no SVG support.
+  if (Modernizr.inlinesvg){
+    $('<a class="nav-button btn" href="#primary-nav"><svg width="15" height="15"><path d="M0,5 15,5" stroke="#FDFFE3" stroke-width="2"/><path d="M0,9 15,9" stroke="#FDFFE3" stroke-width="2"/><path d="M0,13 15,13" stroke="#FDFFE3" stroke-width="2"/></svg></a>').insertBefore('#primary-nav');
+  } else {
+    $('<a class="nav-button btn" href="#primary-nav"><i class="icon-reorder"></i></a>').insertBefore('#primary-nav');
+  }
 
-	// Toggle primary nav with menu button
-	$('.nav-button').on('click', function(){
-	  var target = $(this).attr('href');
-	  $('header[role=banner]').toggleClass('show');
-	  $(target + ' li:not(:first)').toggleClass('hidden');
-	});
+  // Toggle primary nav with menu button
+  $('.nav-button').on('click', function(){
+    var target = $(this).attr('href');
+    $('header[role=banner]').toggleClass('show');
+    $(target + ' li:not(:first)').toggleClass('hidden');
+  });
 
-	// $('article').fitVids();
+  // $('article').fitVids();
 
-	$(function() {
-		var BV = new $.BigVideo(
-			{ container: $('.home-hero') }
-		);
-		BV.init();
-		if (Modernizr.touch) {
-    	BV.show('http://localhost/~james/caocpress/wp-content/themes/caocpress/images/video-poster.jpg');
-		} else {
-			BV.show([
-			  { type: "video/mp4",  src: "http://localhost/~james/caocpress/wp-content/themes/caocpress/video/wt.mp4" },
-			  { type: "video/webm", src: "http://localhost/~james/caocpress/wp-content/themes/caocpress/video/wt.webm" },
-			  { ambient: true }
-			]);
-		}
+  $(function() {
+    var BV = new $.BigVideo({
+      container: $('.home-hero'),
+      doLoop: true,
+      controls: false
+    });
+    BV.init();
+    if (Modernizr.touch) {
+      BV.show('http://localhost/~james/caocpress/wp-content/themes/caocpress/images/video-poster.jpg');
+    } else {
+      BV.show([
+        { type: "video/mp4",  src: "http://localhost/~james/caocpress/wp-content/themes/caocpress/video/wt.mp4" },
+        { type: "video/webm", src: "http://localhost/~james/caocpress/wp-content/themes/caocpress/video/wt.webm" },
+        { ambient: true }
+      ]);
+    }
 
-	});
+  });
 
 
 
@@ -42,12 +44,12 @@ jQuery(document).ready(function($){
 });
 
 if (!Modernizr.touch){
-	$(window).scroll(function(){
-		s = $(window).scrollTop();
-		if (s > 16){
-			$('header[role=banner]').addClass('fixed');
-		} else {
-			$('header[role=banner]').removeClass('fixed');
-		}
-	});
+  $(window).scroll(function(){
+    s = $(window).scrollTop();
+    if (s > 16){
+      $('header[role=banner]').addClass('fixed');
+    } else {
+      $('header[role=banner]').removeClass('fixed');
+    }
+  });
 };
