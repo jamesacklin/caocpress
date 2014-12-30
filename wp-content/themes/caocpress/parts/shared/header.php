31838e731a1511fabcbfacab0851eb62f5912d93
James Acklin <james@acklindesign.com>
2014-12-29 20:45:52 -0500

    Changed logo in header and footer

--- wp-content/themes/caocpress/parts/shared/header.php
+++ wp-content/themes/caocpress/parts/shared/header.php
@@ -1,8 +1,10 @@
   <header role="banner">
     <div class="container-fluid">
       <ul id="primary-nav" class="nav" role="navigation">
-        <li><a href="<?php echo home_url(); ?>" class="logo">
-          <img src="<?= get_template_directory_uri().'/images/logo-combined.svg' ?>" alt="<?php bloginfo( 'name' ); ?>"></a>
+        <li class="home-link">
+          <a href="<?php echo home_url(); ?>" class="logo">
+            <? include(get_template_directory() . '/images/logo-wt.svg'); ?>
+          </a>
         </li>
         <?php
           $menuargs = array(
