b3033da06fa52ba139aba17ddeda5f134b9bb9c6
James Acklin <james@acklindesign.com>
2015-01-18 20:13:38 -0500

    Concat & minify all source libraries

--- wp-content/themes/caocpress/parts/shared/html-footer.php
+++ wp-content/themes/caocpress/parts/shared/html-footer.php
@@ -1,18 +1,4 @@
-
-	<?php wp_footer(); ?>
-
-	<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?= get_template_directory_uri().'/js/vendor/jquery.min.js' ?>"><\/script>')</script>
-	<script src="<?= get_template_directory_uri().'/js/plugins.js' ?>"></script> -->
-
-  <script src="<?= get_template_directory_uri().'/bower_components/jquery/dist/jquery.min.js' ?>"></script>
-  <script src="<?= get_template_directory_uri().'/bower_components/jquery-ui/jquery-ui.min.js' ?>"></script>
-  <script src="<?= get_template_directory_uri().'/bower_components/video.js/dist/video-js/video.js' ?>"></script>
-  <script src="<?= get_template_directory_uri().'/bower_components/eventEmitter/EventEmitter.min.js' ?>"></script>
-  <script src="<?= get_template_directory_uri().'/bower_components/eventie/eventie.js' ?>"></script>
-  <script src="<?= get_template_directory_uri().'/bower_components/imagesloaded/imagesloaded.pkgd.min.js' ?>"></script>
-  <script src="<?= get_template_directory_uri().'/bower_components/bigvideo.js/lib/bigvideo.js' ?>"></script>
-	<script src="<?= get_template_directory_uri().'/js/vendor/jquery.fitvids.js' ?>"></script>
-	<script src="<?= get_template_directory_uri().'/js/main.js' ?>"></script>
+		<?php wp_footer(); ?>
+		<script src="<?= get_template_directory_uri().'/js/min/main-min.js' ?>"></script>
 	</body>
 </html>
